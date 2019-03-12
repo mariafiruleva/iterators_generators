8beb6f54f35236d3a188cd906da6bbaea8b12d08
mariafiruleva <43282413+mariafiruleva@users.noreply.github.com>
2019-03-12 21:33:26 +0300

    edited tracefunc because of incorrect arg number

--- my_own_tracefunc.py
+++ my_own_tracefunc.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def tracefunc(frame, event):
+def tracefunc(frame, event, arg):
     if event == "return":
         print("function:", frame.f_code.co_name, ", local vars:", [*frame.f_locals])
     return tracefunc
