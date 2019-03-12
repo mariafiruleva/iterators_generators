071b19fc90682238d2f8bb8e28625c1db4c1925b
Maria Firulyova <mariafirulevabio@gmail.com>
2019-03-12 14:27:06 +0300

    update benchmarking with memory profiler

--- primes_benchmark.py
+++ primes_benchmark.py
@@ -1,49 +1,44 @@
 import itertools as itertools
 import math as math
-from time import time
-from random import randint
+from time import sleep
 
 
-def benchmark_function_time(defined_function, replicates=1000):
-    """
-    This is a function which can be used for microbenchmarking of primes number generator
-    :return: dictionary with function name, function average time of works and number of replicates
-    """
-    benchmark_info = {'function_name': defined_function.__name__, 'average_time': 0, 'replicates': replicates}
-    for _ in range(replicates):
-        i = randint(900, 1000)
-        time_before_function = time()
-        list(itertools.takewhile(lambda x: x <= i, defined_function()))
-        time_after_function = time()
-        time_diff = time_after_function - time_before_function
-        benchmark_info["average_time"] += time_diff
-    benchmark_info["average_time"] = benchmark_info["average_time"] / replicates
-    return benchmark_info
+@profile
+def benchmark_func_with_factorial(replicates=1500):
+    def primes_one():
+        a = 1
+        while True:
+            a += 1
+            if (math.factorial(a - 1) + 1) % a == 0:
+                yield a
+
+    [list(itertools.takewhile(lambda x: x <= i, primes_one())) for i in range(replicates)]
+    sleep(1)
+    return None
 
 
-def primes_one():
-    a = 1
-    while True:
-        a += 1
-        if (math.factorial(a - 1) + 1) % a == 0:
-            yield a
+@profile
+def benchmark_func_without_factorial(replicates=1500):
+    def primes_two():
+        i = 1
+        while True:
+            count = 0
+            if i <= 2:
+                i += 1
+            else:
+                i += 2
+            for k in range(2, int(i ** 0.5) + 1):
+                if i % k == 0:
+                    count += 1
+                    break
+            if count == 0:
+                yield i
+
+    [list(itertools.takewhile(lambda x: x <= i, primes_two())) for i in range(replicates)]
+    sleep(1)
+    return None
 
 
-def primes_two():
-    i = 1
-    while True:
-        count = 0
-        if i <= 2:
-            i += 1
-        else:
-            i += 2
-        for k in range(2, int(i ** 0.5) + 1):
-            if i % k == 0:
-                count += 1
-                break
-        if count == 0:
-            yield i
-
-
-print(benchmark_function_time(primes_one))
-print(benchmark_function_time(primes_two))
+if __name__ == '__main__':
+    benchmark_func_with_factorial()
+    benchmark_func_without_factorial()
