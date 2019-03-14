491ae6cd0e35636c0e893625021e539aacbfcf6f
mariafiruleva <43282413+mariafiruleva@users.noreply.github.com>
2019-03-14 18:23:14 +0300

    Edited as wrote in slack: add another function

--- my_own_tracefunc.py
+++ my_own_tracefunc.py
@@ -3,6 +3,11 @@ import sys
 
 def tracefunc(frame, event, arg):
     """Tracefunction works correct for Python3.7"""
-    if event == "return" and len([*frame.f_locals]) > 0::
+    if event == "return" and len([*frame.f_locals]) > 0:
         print("function:", frame.f_code.co_name, ", local vars:", [*frame.f_locals])
     return tracefunc
+
+
+def tracefoo(target_function):
+    sys.settrace(tracefunc)
+    target_function()
